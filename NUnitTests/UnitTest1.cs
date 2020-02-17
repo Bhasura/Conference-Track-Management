64b3e7a505fdb079ad8080d21d34c7a58745115f
Bhasura <49043275+Bhasura@users.noreply.github.com>
2020-02-17 14:07:48 +1300

    input 60min returns adde to track

--- NUnitTests/UnitTest1.cs
+++ NUnitTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace NUnitTests
@@ -10,9 +11,47 @@ namespace NUnitTests
         }
 
         [Test]
-        public void Test1()
+        [TestCase("60min", "added to Track")]
+        public void Adding60MinSessionReturnsAddedToTrack(string input, string expected)
         {
-            Assert.Pass();
+            AssertTrue(input, expected);
         }
+        private static void AssertTrue(string input, string expected)
+        {
+            var sut = new TalkEntry();
+            var result = sut.AddToTrack(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+    public class TalkEntry
+    {
+        public string AddToTrack(string newTalk)
+        {
+            string output = string.Empty;
+            if (newTalk.Contains("60min"))
+            {
+                output = "added to Track";
+
+            }
+
+            return output;
+        }
+    }
+    public class Tracks
+    {
+        private List <Sessions> Track1 { get; set; }
+        private List<Sessions> Track2 { get; set; }
+
+    }
+
+    public class Sessions
+    {
+        
+    }
+
+    public class Talks
+    {
+
     }
 }
